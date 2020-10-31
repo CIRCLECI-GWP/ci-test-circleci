b3fea06801e5a5dfb69d6e5b657f203224cccb95
Ndagi Stanley <ndagis@gmail.com>
2020-10-31 05:52:54 +0300

    Add tests

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -58,25 +58,11 @@ class Database extends \CodeIgniter\Database\Config
 	 * @var array
 	 */
 	public $tests = [
-		'DSN'      => '',
-		'hostname' => '127.0.0.1',
+		'hostname' => '',
 		'username' => '',
 		'password' => '',
-		'database' => ':memory:',
-		'DBDriver' => 'SQLite3',
-		'DBPrefix' => 'db_',  // Needed to ensure we're working correctly with prefixes live. DO NOT REMOVE FOR CI DEVS
-		'pConnect' => false,
-		'DBDebug'  => (ENVIRONMENT !== 'production'),
-		'cacheOn'  => false,
-		'cacheDir' => '',
-		'charset'  => 'utf8',
-		'DBCollat' => 'utf8_general_ci',
-		'swapPre'  => '',
-		'encrypt'  => false,
-		'compress' => false,
-		'strictOn' => false,
-		'failover' => [],
-		'port'     => 3306,
+		'database' => '',
+		'DBDriver' => '',
 	];
 
 	//--------------------------------------------------------------------
